d95c77b313c14cc7a0d2f2e1ef87c020134cc866
Glitch (spicy-hospital) <none>
2018-08-13 18:13:21 +0000

    🌱💁 Checkpoint ./script.js:1615526/5356

--- script.js
+++ script.js
@@ -1,77 +1,110 @@
 var hTask = {
-	tasks: [],
-
-	showTasks: function() {
-		if (this.tasks.length === 0) {
-			console.log('You have no items in your To-do List - Time to relax!')
-		} else {
-			console.log('Current Tasks:');
-			for(var i = 0; i < this.tasks.length; i++){
-				if (this.tasks[i].completed === true){
-					console.log('(x)', this.tasks[i].taskText);
-				} else {
-					console.log('( )', this.tasks[i].taskText);
-				}
-			}
-		}
-	},
-	addTask: function(taskText) {
-		this.tasks.push({
-			taskText: taskText,
-			completed: false
-		});
-		this.showTasks(hTask.taskText);
-	},
-	updateTask: function(position, taskText){
-		// this.todos[position] = updatedTask;
-		this.tasks[position].taskText = taskText;
-		this.showTasks();
-	},
-	deleteTask: function(position){
-		this.tasks.splice(position, 1);
-		this.showTasks();
-	},
-	toggleCompletion: function(position) {
-		var task = this.tasks[position];
-		task.completed = !task.completed;
-		this.showTasks();
-	},
-	toggleAll: function(){
-		var taskCount = this.tasks.length;
-		var completedTasks = 0;
-		// GET number of completed todos
-		for(var i = 0; i < taskCount; i++){
-			if (this.tasks[i].completed === true) {
-				completedTasks++;
-			}
-		}
-		// if everything is true, make everything false
-		if(completedTasks === taskCount){
-			for(var i = 0; i < taskCount; i++) {
-				this.tasks[i].completed = false;
-			}
-		} else {
-				// Otherwise make everything true
-				for(var i = 0; i < taskCount; i++){
-					this.tasks[i].completed = true;
-				}
-		}
-		this.showTasks();
-		}
+  tasks: [
+			{taskText: "Eat", completed: false},
+			{taskText: "Dance", completed: false},
+			{taskText: "Be Merry", completed: false}
+		],
+  addTask: function(taskText) {
+    this.tasks.push({taskText: taskText, completed: false});
+  },
+  renameTask: function(position, taskText) {
+    this.tasks[position].taskText = taskText;
+  },
+  removeTask: function(position) {
+    this.tasks.splice(position, 1);
+  },
+  toggleCompleted: function(position) {
+    var task = this.tasks[position];
+    task.completed = !task.completed;
+  },
+  toggleAll: function() {
+    var taskCount = this.tasks.length;
+    var completedTasks = 0;
+    for (var i = 0; i < taskCount; i++) {
+      if (this.tasks[i].completed === true) {
+        completedTasks++;
+      }
+    }
+    if (completedTasks === taskCount) {
+      for (var i = 0; i < taskCount; i++) {
+        this.tasks[i].completed = false;
+      }
+    } else {
+      for (var i = 0; i < taskCount; i++) {
+        this.tasks[i].completed = true;
+      }
+    }
+  }
 };
 
-// Targetting Javascript using HTML
-var showTasksButton = document.getElementById('showTasksButton');
-console.log(showTasksButton)
-	// Run Display Tasks method after click of above button
-	showTasksButton.addEventListener('click', function(){
-		hTask.showTasks();
-	});
+var handlers = {
+	addTask: function(){
+		var addTaskInput = document.getElementById('addTaskInput');
+		hTask.addTask(addTaskInput.value);
+		addTaskInput.value = '';
+		view.displayTasks();
+	},
+	renameTask: function() {
+		var renameTaskText = document.getElementById('renameTaskText');
+		var renameTaskPosition = document.getElementById('renameTaskPosition');
 
-// Targetting Javascript using HTML
-var completeTasksButton = document.getElementById('completeTasksButton');
-console.log(completeTasksButton)
-	// Run Display Tasks method after click of above button
-	completeTasksButton.addEventListener('click', function(){
-		hTask.toggleAll();
-	});
+		hTask.renameTask(renameTaskPosition.valueAsNumber, renameTaskText.value);
+		renameTaskPosition.value = '';
+		renameTaskText.value = '';
+		view.displayTasks();
+	},
+	removeTask: function() {
+		var removeTaskPosition = document.getElementById('removeTaskPosition');
+		hTask.removeTask(removeTaskPosition.valueAsNumber);
+		removeTaskPosition.value = '';
+		view.displayTasks();
+	},
+	toggleAll: function() {
+  	hTask.toggleAll();
+	},
+	toggleCompleted: function() {
+		var toggleCompletedPosition = document.getElementById('toggleCompletedPosition');
+		hTask.toggleCompleted(toggleCompletedPosition.valueAsNumber);
+		toggleCompletedPosition.value = '';
+		view.displayTasks();
+	}
+};
+
+// Adding the tasks into the Apps Body (HTML)
+
+// this is the object that contains all the view methods
+var view = {
+	// this is the method that displays our current tasks to the HTML page
+	displayTasks: function(){
+			// This line create the container for the list items
+			var tasksUl = document.querySelector('ul');
+			// This wipes the Whole List on page load
+			tasksUl.innerHTML = '';
+			for(var i = 0; i < hTask.tasks.length; i++){
+				// This line creates the blank Li Element for the HTML page
+				var taskLi = document.createElement('li');
+				var task = hTask.tasks[i];
+				var taskIsCompleted = '';
+				// (x) taskText
+				// var taskTextCompleted = '';
+
+				//if(taskCompleted === true)
+					// (x) taskText
+					// else
+					// () taskText
+
+					// taskLi.textContent = taskTextCompleted;
+				if (hTask.tasks === 0){
+					return "Your Task list is empty!"
+				} else if (task.completed === true) {
+					// This tells the computer what to put in the list item
+					taskIsCompleted = '(x) ' + task.taskText;
+				} else {
+					taskIsCompleted = '( ) ' + task.taskText;
+				}
+				taskLi.textContent = taskIsCompleted;
+				// This line below, tells the system what to do with the list item
+				tasksUl.appendChild(taskLi);
+		}
+	}
+};
