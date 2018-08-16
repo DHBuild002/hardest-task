47d8b83a3883a1f756e6827f14fb300fa7101d8b
Glitch (the-hardest-task-v9) <none>
2018-08-16 12:37:59 +0000

    💧📟 Checkpoint ./script.js:1615526/2836 ./index.html:1615526/120

--- script.js
+++ script.js
@@ -102,29 +102,34 @@ var view = {
 				tasksUl.appendChild(taskLi);
 		}
   },
-      addRemoveButton: function(){
+    addRemoveButton: function(){
         var removeButton = document.createElement('button');
         removeButton.textContent = 'Remove Task';
         removeButton.className = 'removeButton';
         return removeButton;
-      }
+      },
+    createEventListeners: function(){
+      // Setting up an Event listener to the whole Unordered list
+      var ulEvents = document.querySelector('ul');
+
+        ulEvents.addEventListener('click', function(event) {
+        // Line Below: Targets the Click event inside of the UL then gives you all the dom information about that click event
+        // Using one of the properties named ParentNode, you can access the ID in the parenting LI element
+        // This enables us to both detect a click in the general UL, and target the specific item in the list
+        console.log(event.target.parentNode.id);
+
+        // Get the element that was clicked one
+        var elementClicked = event.target;
+        // check the element clicked was the delete button
+        if (elementClicked.className === 'removeButton'){
+            handlers.removeTask(parseInt(elementClicked.parentNode.id));
+            // Getting the position of the task we wish to remove.
+            // This then gets put into the perntheses above to act as the position - parseInt(elementClicked.parentNode.id);
+        }
+      });
+    }
 	};
 
-// Setting up an Event listener to the whole Unordered list
-var ulEvents = document.querySelector('ul');
-
-  ulEvents.addEventListener('click', function(event) {
-  // Line Below: Targets the Click event inside of the UL then gives you all the dom information about that click event
-  // Using one of the properties named ParentNode, you can access the ID in the parenting LI element
-  // This enables us to both detect a click in the general UL, and target the specific item in the list
-  console.log(event.target.parentNode.id);
-  
-  // Get the element that was clicked one
-  var elementClicked = event.target;
-  // check the element clicked was the delete button
-  if (elementClicked.className === 'removeButton'){
-      handlers.removeTask(parseInt(elementClicked.parentNode.id));
-      // Getting the position of the task we wish to remove.
-      // This then gets put into the perntheses above to act as the position - parseInt(elementClicked.parentNode.id);
-  }
-});
+// The ulEvents event listener was merged into a new method as part of the view object - Better Organisation
+// Call the createEventListeners() method from the view object
+view.createEventListeners();
