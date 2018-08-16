350143d7f85b86cfd562ddbb0726234773553582
Glitch (the-hardest-task-v9) <none>
2018-08-16 12:30:45 +0000

    😬💍 Checkpoint ./script.js:1615526/242

--- script.js
+++ script.js
@@ -111,23 +111,20 @@ var view = {
 	};
 
 // Setting up an Event listener to the whole Unordered list
-var eventsInUl = document.querySelector('ul');
-eventsInUl.addEventListener('click', function(event) {
+var ulEvents = document.querySelector('ul');
+
+  ulEvents.addEventListener('click', function(event) {
   // Line Below: Targets the Click event inside of the UL then gives you all the dom information about that click event
   // Using one of the properties named ParentNode, you can access the ID in the parenting LI element
   // This enables us to both detect a click in the general UL, and target the specific item in the list
-  // console.log(event.target.parentNode.id);
-  
-  
+  console.log(event.target.parentNode.id);
   
   // Get the element that was clicked one
   var elementClicked = event.target;
   // check the element clicked was the delete button
-  if ( elementClicked.className === 'deleteButton'){
-      handlers.removeTask();
+  if (elementClicked.className === 'removeButton'){
+      handlers.removeTask(parseInt(elementClicked.parentNode.id));
       // Getting the position of the task we wish to remove.
-      parseInt(elementClicked.parentNode.id);
-  
+      // This then gets put into the perntheses above to act as the position - parseInt(elementClicked.parentNode.id);
   }
-  
 });
